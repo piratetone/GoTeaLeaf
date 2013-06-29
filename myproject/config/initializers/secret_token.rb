b9191063e33e73dfa4ad35e32e469b91c2e8b1e4
Gary Spagnoli <gary.spagnoli@gmail.com>
2013-06-29 14:36:32 -0400

    First Commit
    Here is my first batch of test code for the first week of homework for
    the GoTeaLeaf course

---
+++ myproject/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Myproject::Application.config.secret_token = '3804ef255ef6d7b652a5fbe8c178e0838c480e67bea6b26abe67207a5da787812efb0c7263ac1fbb614874245c54c17b9b5575a19d7cc53d400ac559c3a7a795'
